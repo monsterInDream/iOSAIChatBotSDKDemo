8476326950bf9fce96e35010ec8f033f8c33ea3d
ZhangYe <728815221@qq.com>
2025-02-06 16:06:06 +0800

    Bug fix or small change

--- iOSAIChatBotSDK.podspec
+++ iOSAIChatBotSDK.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'iOSAIChatBotSDK'
-  s.version          = '1.0.2'
+  s.version          = '1.0.3'
   s.summary          = 'iOSAIChatBotSDK is a chatbot SDK for iOS applications, providing AI-driven conversation capabilities.'
 
 # This description is used to generate tags and improve search results.
--- iOSAIChatBotSDK/Classes/InitializationManager/InitializationManager.swift
+++ iOSAIChatBotSDK/Classes/InitializationManager/InitializationManager.swift
@@ -3,8 +3,9 @@ import Foundation
 
 //在这个页面中设置验证身份,必须先设置身份后，在能继续下一步
 
-//1.将身份Key放在本地中：
+//1.将身份Key存放在本地中：
 public func setupYourAppKey(appKey: String, success: @escaping()->(), fail: @escaping(String)->()){
+    print("将身份Key存放在本地中")
     if appKey.count <= 0{
         fail("appky is not avisible")
         return
@@ -16,12 +17,14 @@ public func setupYourAppKey(appKey: String, success: @escaping()->(), fail: @esc
 
 //2.清除本地的Key
 public func deleteYourAppKey(){
+    print("清除本地的Key")
     UserDefaults.standard.set("", forKey: "iOSAIChatBotSDK_InitializationManager_LocalAppKey")
     UserDefaults.standard.synchronize()
 }
 
 //3.获取本地的key
 public func getYourAppKey() -> String{
+    print("获取本地的key")
     guard let local_key = UserDefaults.standard.value(forKey: "iOSAIChatBotSDK_InitializationManager_LocalAppKey") as? String else{
         return ""
     }
@@ -30,4 +33,5 @@ public func getYourAppKey() -> String{
     }else{
         return ""
     }
+   
 }
